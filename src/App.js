1a84b97ed48b6efdc5081651409f226e08784a7f
Abdullah <abdullahkhrief@gmail.com>
2023-05-09 19:21:36 +0300

    About content has been added and Routes has been installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quote />
-    </div>
+    <>
+    <About/>
+    <Routes>
+      <Route path='/calculator' element={<Calculator/>}/>
+      <Route path='/quote' element ={<Quote />} />
+      </Routes>
+      </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+const About = () => {
+    return<>
+    <h3>Welcome to the page</h3>
+    <p>The page is based on React and React Routes And it is Single Page
+         Application(SPA) and it has three pages which has a About, Calculato, 
+         and Quotes[API] pages, you can press in the Navigation bar to chang
+         the page content.
+          </p>
+          <br></br>
+          <p>Abdullah AL-Khureif</p>
+    </>
+}
+
+export default About
